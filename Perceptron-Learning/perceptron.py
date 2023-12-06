b9e146ebe3853a242d09dd5b8ad26da701965ffd
Anthony Tong <atong28.usa@gmail.com>
2023-12-06 14:40:07 -0800

    Add probit data generation

--- Perceptron-Learning/perceptron.py
+++ Perceptron-Learning/perceptron.py
@@ -17,7 +17,7 @@ def sigmoid(z):
 #   Probabilities result from applying sigmoid to the z array
 #   Theta is drawn from N(0,1) and normalized to a given value of beta
 #   X is drawn randomly from U[-1, 1]
-def generate_data(beta, n, seed):
+def generate_logit_data(beta, n, seed):
     np.random.seed(seed)
     theta_gen = np.random.normal(size=(NUM_FEATURES, 1))
     theta_gen = theta_gen * beta / np.linalg.norm(theta_gen)
@@ -26,9 +26,18 @@ def generate_data(beta, n, seed):
     y_gen = np.random.binomial(1, sigmoid(z_gen))
     return theta_gen, X_gen, np.ndarray.flatten(y_gen)
 
+def generate_probit_data(beta, n, seed):
+    np.random.seed(seed)
+    theta_gen = np.random.normal(size=(NUM_FEATURES, 1))
+    theta_gen = theta_gen * beta / np.linalg.norm(theta_gen)
+    X_gen = np.random.normal(0, 1, (n, NUM_FEATURES))
+    z_gen = np.dot(X_gen, theta_gen)
+    y_gen = (np.sign(z_gen + np.random.normal(0, 1, (np.shape(z_gen)))) + 1) / 2
+    return theta_gen, X_gen, np.ndarray.flatten(y_gen)
+
 if __name__ == '__main__':
-    beta = 50
-    theta, X, y = generate_data(beta, 20000, 1)
+    beta = 0.5
+    theta, X, y = generate_probit_data(beta, 20000, 1)
     model = Perceptron(fit_intercept=False)
     model.fit(X[:10000], y[:10000])
 
@@ -38,7 +47,12 @@ if __name__ == '__main__':
     print(model.score(X[10000:], y[10000:]))
     print(logit_model.score(X[10000:], y[10000:]))
 
-    print(np.linalg.norm(model.coef_))
-    print(np.linalg.norm(logit_model.coef_))
+    dir_error = np.linalg.norm(model.coef_/np.linalg.norm(model.coef_) - logit_model.coef_/np.linalg.norm(logit_model.coef_))
+    len_error = np.linalg.norm(model.coef_) - np.linalg.norm(logit_model.coef_)/1.65
+
+    print(f'Directional error: {dir_error}')
+    print(f'Length error: {len_error}')
+
+    print(f'Real: {np.linalg.norm(theta)} | Probit*1.65: {np.linalg.norm(model.coef_) * 1.65} | Logit: {np.linalg.norm(logit_model.coef_)}')
     # direction_error = np.linalg.norm(theta / beta - model.coef_ / np.linalg.norm(model.coef_))
     # magnitude_error = (beta - np.linalg.norm(model.coef_)) / beta
