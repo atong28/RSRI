3bf37d6939eb8b0f929bad77201786760847929f
Anthony Tong <atong28.usa@gmail.com>
2023-12-07 15:47:53 -0800

    add plots for perceptron

--- Perceptron-Learning/perceptron.py
+++ Perceptron-Learning/perceptron.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
 import json
 import sys
 
 # Constants
-NUM_FEATURES = 2
+NUM_FEATURES = 100
 EPSILON = 0.5
 
 # Calculate the sigmoid / logistic function
@@ -36,23 +37,68 @@ def generate_probit_data(beta, n, seed):
     return theta_gen, X_gen, np.ndarray.flatten(y_gen)
 
 if __name__ == '__main__':
-    beta = 0.5
-    theta, X, y = generate_probit_data(beta, 20000, 1)
-    model = Perceptron(fit_intercept=False)
-    model.fit(X[:10000], y[:10000])
+    '''
+    n = 10000
+    
 
-    logit_model = LogisticRegression(fit_intercept=False, penalty=None)
-    logit_model.fit(X[:10000], y[:10000])
+    beta_values = list(range(1, 40))
+    p_scores = []
+    l_scores = []
 
-    print(model.score(X[10000:], y[10000:]))
-    print(logit_model.score(X[10000:], y[10000:]))
+    for beta in beta_values:
+        theta, X, y = generate_logit_data(beta, n+5000, 1)
+        probit_model = Perceptron(fit_intercept=False)
+        probit_model.fit(X[:n], y[:n])
 
-    dir_error = np.linalg.norm(model.coef_/np.linalg.norm(model.coef_) - logit_model.coef_/np.linalg.norm(logit_model.coef_))
-    len_error = np.linalg.norm(model.coef_) - np.linalg.norm(logit_model.coef_)/1.65
+        logit_model = LogisticRegression(fit_intercept=False, penalty=None)
+        logit_model.fit(X[:n], y[:n])
 
-    print(f'Directional error: {dir_error}')
-    print(f'Length error: {len_error}')
+        p_scores.append(probit_model.score(X[n:], y[n:]))
+        l_scores.append(logit_model.score(X[n:], y[n:]))
 
-    print(f'Real: {np.linalg.norm(theta)} | Probit*1.65: {np.linalg.norm(model.coef_) * 1.65} | Logit: {np.linalg.norm(logit_model.coef_)}')
+    pline, = plt.plot(beta_values, p_scores)
+    lline, = plt.plot(beta_values, l_scores)
+
+    plt.xlabel("n")
+    plt.ylabel("Accuracy")
+    plt.legend([lline, pline], ["Logit model", "Probit model"])
+    plt.show()
+    '''
+    #'''
+    beta = 40
+    theta, X, y = generate_logit_data(beta, 30000, 1)
+
+    n_values = list(range(1000, 25000, 250))
+    p_scores = []
+    l_scores = []
+
+    for n in n_values:
+
+        probit_model = Perceptron(fit_intercept=False)
+        probit_model.fit(X[:n], y[:n])
+
+        logit_model = LogisticRegression(fit_intercept=False, penalty=None, max_iter=1000)
+        logit_model.fit(X[:n], y[:n])
+
+        p_scores.append(probit_model.score(X[25000:], y[25000:]))
+        l_scores.append(logit_model.score(X[25000:], y[25000:]))
+
+    pline, = plt.plot(n_values, p_scores)
+    lline, = plt.plot(n_values, l_scores)
+
+    plt.xlabel("n")
+    plt.ylabel("Accuracy")
+    plt.legend([lline, pline], ["Logit model", "Probit model"])
+    plt.show()
+    #'''
+
+
+    # dir_error = np.linalg.norm(model.coef_/np.linalg.norm(model.coef_) - logit_model.coef_/np.linalg.norm(logit_model.coef_))
+    # len_error = np.linalg.norm(model.coef_) - np.linalg.norm(logit_model.coef_)/1.65 
+
+    # print(f'Directional error: {dir_error}')
+    # print(f'Length error: {len_error}')
+
+    # print(f'Real: {np.linalg.norm(theta)} | Probit*1.65: {np.linalg.norm(model.coef_) * 1.65} | Logit: {np.linalg.norm(logit_model.coef_)}')
     # direction_error = np.linalg.norm(theta / beta - model.coef_ / np.linalg.norm(model.coef_))
     # magnitude_error = (beta - np.linalg.norm(model.coef_)) / beta
