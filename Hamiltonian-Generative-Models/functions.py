73caaafcf3009ba8e1bb9e4962d695b07377a3fd
Anthony Tong <atong28.usa@gmail.com>
2023-12-21 22:16:32 -0800

    clean up hamiltonian code

---
+++ Hamiltonian-Generative-Models/functions.py
@@ -0,0 +1,173 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import matplotlib.pyplot as plt
+import torchvision
+from torch.utils.data import Dataset
+from tqdm.notebook import tqdm
+import cv2
+import os
+from IPython.display import clear_output
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+########################################################################################
+# Calculates the etas, alphas, and betas given a constant eta_const.                   #
+########################################################################################
+def get_parameters(eta_const, num_timesteps):
+    etas = torch.ones(num_timesteps+1) * eta_const
+    alphas = torch.cat((torch.tensor([1.0]), torch.cumprod(torch.cos(etas[:1000]), 0)))
+    betas = 1 - (alphas ** 2)
+    
+    return etas, alphas, betas
+
+########################################################################################
+# Creates a dummy dataset of uniform values as images.                                 #
+########################################################################################
+class UniformDataset(Dataset):
+    def __init__(self, num_samples, image_size=32, step_size=0.2):
+        self.num_samples = num_samples
+        self.image_size = image_size
+        self.step_size = step_size
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        # Generate a random integer in the range [-5, 5] and multiply by step_size
+        value = torch.randint(low=-5, high=6, size=(1,)).float() * self.step_size
+        data_point = torch.ones((1, self.image_size, self.image_size)) * value
+
+        # Classify into a class from 0 to 10 based on the generated value
+        label = int((value + 1) / self.step_size)
+
+        return data_point, label
+
+########################################################################################
+# Given a list of images (tensors) produce a grid with images. The image is saved to a #
+# file if provided, and a title can be specified. For best results the length of the   #
+# list should be a perfect square.                                                     #
+########################################################################################
+def show_images(images, file="", title=""):
+    """Shows the provided images as sub-pictures in a square"""
+    images = [im.permute(1,2,0).numpy() for im in images]
+
+    # Defining number of rows and columns
+    fig = plt.figure(figsize=(8, 8))
+    rows = int(len(images) ** (1 / 2))
+    cols = round(len(images) / rows)
+
+    # Populating figure with sub-plots
+    idx = 0
+    for r in range(rows):
+        for c in range(cols):
+            fig.add_subplot(rows, cols, idx + 1)
+
+            if idx < len(images):
+                plt.imshow(images[idx], cmap="gray", vmin=-1, vmax=1)
+                plt.axis('off')
+                idx += 1
+
+    # Showing the figure
+    fig.suptitle(title, fontsize=30)
+    plt.show()
+    if file != "":
+        fig.savefig(file)
+
+########################################################################################
+# Generates images given the Hamiltonian model. Provide the number of images to        #
+# generate, and also the number of color channels & size of image.                     #
+########################################################################################   
+def generate_image(hamiltonian, sample_size, channel, size):
+    """Generate the image from the Gaussian noise"""
+
+    frames = []
+    hamiltonian.eval()
+    with torch.no_grad():
+        timesteps = list(range(hamiltonian.num_timesteps))[::-1]
+        sample = torch.randn(sample_size, channel, size, size).to(device)
+        
+        for i, t in enumerate(tqdm(timesteps)):
+            frame = []
+            time_tensor = (torch.ones(sample_size,1) * t).long().to(device)
+            residual = hamiltonian.reverse(sample, time_tensor)
+            sample = hamiltonian.step(residual, time_tensor[0], sample)
+            for i in range(sample_size):
+                frame.append(sample[i].detach().cpu())
+            frames.append(frame)
+    return frames
+
+########################################################################################
+# Generates a video provided images, timesteps, and a file title and step size.        #
+########################################################################################
+def generate_video(images, num_timesteps, file_title='generation' step=5):
+    # Generate Images with Matplotlib
+    for i in range(0, num_timesteps, step):
+        show_images(generated[i], file=f'images/{i}.png', title=f'k={i}')
+        clear_output(wait=True)
+
+    # Create Frames List
+    img_array = []
+    for i in range(0, num_timesteps, 5):
+        img = cv2.imread(f'images/{i}.png')
+        height, width, layers = img.shape
+        size = (width,height)
+        img_array.append(img)
+
+    # Define VideoWriter
+    out = cv2.VideoWriter(f'videos/{file_title}.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 15, size)
+
+    # Generate Video
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+
+    # Clear image directory to prevent clutter
+    for root, dirs, files in os.walk('images'):
+        for file in files:
+            if file.endswith('.png'):
+                os.remove(os.path.join(root, file))
+
+########################################################################################
+# Training loop for training the UNet/HGM.                                             #
+########################################################################################
+def training_loop(model, dataloader, optimizer, num_epochs, num_timesteps, device=device):
+    """Training loop for DDPM"""
+
+    global_step = 0
+    losses = []
+
+    for epoch in range(num_epochs):
+        model.train()
+        progress_bar = tqdm(total=len(dataloader))
+        progress_bar.set_description(f"Epoch {epoch}")
+        for step, batch in enumerate(dataloader):
+            batch = batch[0].to(device)
+            noise = torch.randn(batch.shape).to(device)
+            timesteps = torch.randint(0, num_timesteps, (batch.shape[0],)).long().to(device)
+
+            noisy = model.add_noise(batch, noise, timesteps)
+            noise_pred = model.reverse(noisy, timesteps)
+            loss = F.mse_loss(noise_pred, noise)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            progress_bar.update(1)
+            logs = {"loss": loss.detach().item(), "step": global_step}
+            losses.append(loss.detach().item())
+            progress_bar.set_postfix(**logs)
+            global_step += 1
+
+        progress_bar.close()
+        clear_output(wait=True)
+
+########################################################################################
+# Load a saved .pt model file.                                                         #
+########################################################################################
+def load_model(in_channels=1, filename):
+    network = UNet(in_channels=in_channels)
+    network = network.to(device)
+    model = Hamiltonian(network, num_timesteps, device=device)
+    model.load_state_dict(torch.load(f'{filename}')['model_state_dict'])
+    return model
---
+++ Hamiltonian-Generative-Models/model.py
@@ -0,0 +1,167 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from tqdm.notebook import tqdm
+from torch.utils.data import Subset
+import copy
+
+def sinusoidal_embedding(n, d):
+    # Returns the standard positional embedding
+    embedding = torch.tensor([[i / 10_000 ** (2 * j / d) for j in range(d)] for i in range(n)])
+    sin_mask = torch.arange(0, n, 2)
+    embedding[sin_mask] = torch.sin(embedding[sin_mask])
+    embedding[1 - sin_mask] = torch.cos(embedding[sin_mask])
+    return embedding
+
+class double_conv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class down_layer(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(down_layer, self).__init__()
+        self.pool = nn.MaxPool2d(2, stride=2, padding=0)
+        self.conv = double_conv(in_ch, out_ch)
+
+    def forward(self, x):
+        x = self.conv(self.pool(x))
+        return x
+
+class up(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(up, self).__init__()
+        self.up_scale = nn.ConvTranspose2d(in_ch, out_ch, 2, stride=2)
+
+    def forward(self, x1, x2): # x1 (bs,out_ch,w1,h1) x2 (bs,in_ch,w2,h2)
+        x2 = self.up_scale(x2) # (bs,out_ch,2*w2,2*h2)
+        diffY = x1.size()[2] - x2.size()[2]
+        diffX = x1.size()[3] - x2.size()[3]
+
+        x2 = F.pad(x2, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2]) # (bs,out_ch,w1,h1)
+        x = torch.cat([x2, x1], dim=1) # (bs,2*out_ch,w1,h1)
+        return x
+
+class up_layer(nn.Module):
+    def __init__(self, in_ch, out_ch): # !! 2*out_ch = in_ch !!
+        super(up_layer, self).__init__()
+        self.up = up(in_ch, out_ch)
+        self.conv = double_conv(in_ch, out_ch)
+
+    def forward(self, x1, x2): # x1 (bs,out_ch,w1,h1) x2 (bs,in_ch,w2,h2)
+        a = self.up(x1, x2) # (bs,2*out_ch,w1,h1)
+        x = self.conv(a) # (bs,out_ch,w1,h1) because 2*out_ch = in_ch
+        return x
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=1, n_steps=1000, time_emb_dim=100):
+        super(UNet, self).__init__()
+        self.conv1 = double_conv(in_channels, 64)
+        self.down1 = down_layer(64, 128)
+        self.down2 = down_layer(128, 256)
+        self.down3 = down_layer(256, 512)
+        self.down4 = down_layer(512, 1024)
+        self.up1 = up_layer(1024, 512)
+        self.up2 = up_layer(512, 256)
+        self.up3 = up_layer(256, 128)
+        self.up4 = up_layer(128, 64)
+        self.last_conv = nn.Conv2d(64, in_channels, 1)
+        
+        # Time embedding
+        self.time_embed = nn.Embedding(n_steps, time_emb_dim)
+        self.time_embed.weight.data = sinusoidal_embedding(n_steps, time_emb_dim)
+        self.time_embed.requires_grad_(False)
+        self.te1 = self._make_te(time_emb_dim, in_channels)
+        self.te2 = self._make_te(time_emb_dim, 64)
+        self.te3 = self._make_te(time_emb_dim, 128)
+        self.te4 = self._make_te(time_emb_dim, 256)
+        self.te5 = self._make_te(time_emb_dim, 512)
+        self.te1_up = self._make_te(time_emb_dim, 1024)
+        self.te2_up = self._make_te(time_emb_dim, 512)
+        self.te3_up = self._make_te(time_emb_dim, 256)
+        self.te4_up = self._make_te(time_emb_dim, 128)
+
+    def _make_te(self, dim_in, dim_out):
+        return nn.Sequential(nn.Linear(dim_in, dim_out), nn.SiLU(), nn.Linear(dim_out, dim_out))
+    
+    def forward(self, x , t): # x (bs,in_channels,w,d)
+        bs = x.shape[0]
+        t = self.time_embed(t)
+        x1 = self.conv1(x+self.te1(t).reshape(bs, -1, 1, 1)) # (bs,64,w,d)
+        x2 = self.down1(x1+self.te2(t).reshape(bs, -1, 1, 1)) # (bs,128,w/2,d/2)
+        x3 = self.down2(x2+self.te3(t).reshape(bs, -1, 1, 1)) # (bs,256,w/4,d/4)
+        x4 = self.down3(x3+self.te4(t).reshape(bs, -1, 1, 1)) # (bs,512,w/8,h/8)
+        x5 = self.down4(x4+self.te5(t).reshape(bs, -1, 1, 1)) # (bs,1024,w/16,h/16)
+        x1_up = self.up1(x4, x5+self.te1_up(t).reshape(bs, -1, 1, 1)) # (bs,512,w/8,h/8)
+        x2_up = self.up2(x3, x1_up+self.te2_up(t).reshape(bs, -1, 1, 1)) # (bs,256,w/4,h/4)
+        x3_up = self.up3(x2, x2_up+self.te3_up(t).reshape(bs, -1, 1, 1)) # (bs,128,w/2,h/2)
+        x4_up = self.up4(x1, x3_up+self.te4_up(t).reshape(bs, -1, 1, 1)) # (bs,64,w,h)
+        output = self.last_conv(x4_up) # (bs,in_channels,w,h)
+        return output
+
+class Hamiltonian(nn.Module):
+    def __init__(self, network, num_timesteps, eta_start=0.0001, eta_end=0.02, device=device, etas=None, alphas=None, betas=None) -> None:
+        super(Hamiltonian, self).__init__()
+        self.num_timesteps = num_timesteps
+        if etas is None:
+            self.etas = torch.linspace(eta_start, eta_end, num_timesteps, dtype=torch.float32).to(device)
+            self.alphas = torch.cumprod(torch.cos(self.etas), 0)
+            self.betas = 1-(self.alphas ** 2)
+        else:
+            self.etas=etas.to(device)
+            self.alphas=alphas.to(device)
+            self.betas=betas.to(device)
+        self.network = network
+        self.device = device
+
+    def add_noise(self, x_start, x_noise, timesteps):
+        # The forward process
+        # x_start and x_noise (bs, n_c, w, d)
+        # timesteps (bs)
+        s1 = self.alphas[timesteps] # bs
+        s2 = self.betas[timesteps] ** 0.5 # bs
+        s1 = s1.reshape(-1,1,1,1) # (bs, 1, 1, 1) for broadcasting
+        s2 = s2.reshape(-1,1,1,1) # (bs, 1, 1, 1)
+        return s1 * x_start + s2 * x_noise
+
+    def reverse(self, x, t):
+        # The network return the estimation of the noise we added
+        return self.network(x, t)
+
+    def step(self, model_output, timestep, sample):
+        # one step of sampling
+        # timestep (1)
+        k = timestep
+        tan_sq_eta = torch.tan(self.etas[k]) ** 2
+        alpha_ratio = self.alphas[k-1] / self.alphas[k]
+        s1 = (self.betas[k-1] + (alpha_ratio * tan_sq_eta)) / (tan_sq_eta + self.betas[k-1])
+        s2 = (alpha_ratio * (self.betas[k] ** 0.5) * tan_sq_eta) / (tan_sq_eta + self.betas[k-1])
+        s1 = s1.reshape(-1,1,1,1)
+        s2 = s2.reshape(-1,1,1,1)
+        pred_prev_sample = s1 * sample - s2 * model_output
+
+        variance = 0
+        if k > 0:
+            noise = torch.randn_like(model_output).to(self.device)
+            variance = (((tan_sq_eta * self.betas[k-1]) / (tan_sq_eta + self.betas[k-1])) ** 2) * (noise)
+
+        pred_prev_sample = pred_prev_sample + variance
+
+        return pred_prev_sample
